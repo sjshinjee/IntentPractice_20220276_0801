493b44f904bb0eba70db781d3b8b7bf7b4c76fa7
신지 <wmfrjdnjfk12@gmail.com>
2022-08-01 17:53:08 +0900

    클래스 Intent(1) 돌아가기까지 마침 / Intent는 자료구조상 stact을 쌓아주는 형태기 때문에 앱에 과부하를 초과할수 있다 그래서 그냥 finish함수를 쓴다

--- app/src/main/java/com/homee/intentpractice_20220276_0801/SecondActivity.kt
+++ app/src/main/java/com/homee/intentpractice_20220276_0801/SecondActivity.kt
@@ -1,11 +1,18 @@
 package com.homee.intentpractice_20220276_0801
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        backBtn.setOnClickListener {
+ //           val myIntent = Intent(this, MainActivity::class.java)
+   //         startActivity(myIntent)
+            finish()
+        }
     }
 }
