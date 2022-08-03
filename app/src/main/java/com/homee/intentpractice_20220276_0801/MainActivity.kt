a422c8f16ea05c87c79fe8dd1a52d6613e46fe22
신지 <wmfrjdnjfk12@gmail.com>
2022-08-03 16:53:44 +0900

    Intetnt(3)
    1 A화면 -> B화면으로 이동
    2 B화면 데이터 첨부 후 A화면으로 이동
    3 A화면에서 수정된 데이터 수령후 UI반영

--- app/src/main/java/com/homee/intentpractice_20220276_0801/MainActivity.kt
+++ app/src/main/java/com/homee/intentpractice_20220276_0801/MainActivity.kt
@@ -21,10 +21,24 @@ class MainActivity : AppCompatActivity() {
 
         editBtn.setOnClickListener {
             val myIntent = Intent(this, EditActivity::class.java)
-            startActivityForResult(myIntent,1000)
+            startActivityForResult(myIntent, 1000)
 
         }
+    }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)  // requestCode:출발목적코드  resultCode:실제수정여부확인코드
+        //data:수정된 데이터 첨부된 intent
+        //그리고 if문으로 분기처리를 해줘야 함/ 가장 먼저 분기처리를 해 줘야하는 것은 실제로 수정이 안료되었는지 부터 물어
+        if (resultCode == RESULT_OK){
+            if (requestCode == 1000){
+                //then 수정된 데이터를 바탕으로 UI반영
+                val name = data?.getStringExtra("name")
+                val phoneNum = data?.getStringExtra("phoneNum")
+                nameTxt.text = name
+                phoneNumTxt.text = phoneNum
+            }
         }
+    }
     }
