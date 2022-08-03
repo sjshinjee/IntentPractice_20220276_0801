2dba5fd28d65a6936d7a083cef5a03b49dec939f
신지 <wmfrjdnjfk12@gmail.com>
2022-08-03 15:31:37 +0900

    Intetnt(3)
    1 A화면 -> B화면으로 이동
    2 B화면 데이터 첨부 후 A화면으로 이동

--- app/src/main/java/com/homee/intentpractice_20220276_0801/EditActivity.kt
+++ app/src/main/java/com/homee/intentpractice_20220276_0801/EditActivity.kt
@@ -1,11 +1,24 @@
 package com.homee.intentpractice_20220276_0801
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        finishBtn.setOnClickListener {
+            val name = nameEdt.text.toString()
+            val phoneNum = phoneEdt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("name",name)
+                .putExtra("phoneNum", phoneNum)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
