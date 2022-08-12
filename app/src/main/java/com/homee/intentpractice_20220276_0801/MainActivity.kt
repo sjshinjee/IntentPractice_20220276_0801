d4ce7f9a8e1e9477649aa6321920506418b92381
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-12 18:11:09 +0900

    가상화면 안 움직였던거 해결 at 8/12 &설명 많이 붙였다

--- app/src/main/java/com/homee/intentpractice_20220276_0801/MainActivity.kt
+++ app/src/main/java/com/homee/intentpractice_20220276_0801/MainActivity.kt
@@ -21,11 +21,12 @@ class MainActivity : AppCompatActivity() {
 
                 editBtn.setOnClickListener {
                     val myIntent = Intent(this, EditActivity::class.java)
-                    startActivityForResult(myIntent,1000)  //1000은 requestCode 출발 목적코드다 coz 수정화면이 실제로는 많으므로
+                    startActivityForResult(myIntent,100)  //1000은 requestCode 출발 목적코드다 coz 수정화면이 실제로는 많으므로
+                                                           // 실무에서는 위에 val REQ_FOR_EDIT처럼 변수를 주고 1000대신에 그 변수명을 써준다
                                 }
              }
 
-    }
+
     //수정한 데이터 확인 후  UI반영
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {   //onActivityReult누르고 자동완성 시켜주면 나옴
         super.onActivityResult(requestCode, resultCode, data)  //여기까지 자동완성 된다 requestCode:출발목적코드  resultCode:실제수정여부확인코드
@@ -35,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         //실제로 수정이 이뤄졌는가
         if (resultCode == RESULT_OK){
             //출발 목적이 맞는가
-            if (requestCode == 1000){
+            if (requestCode == 100){
                 //then 수정된 데이터를 바탕으로 UI반영
                 val name = data?.getStringExtra("name")  //이 친구는 null값이 가능하므로 error가 뜨므로 일단 ?넣어서 해결 7/27일에 수업할 거임
                 val phoneNum = data?.getStringExtra("phoneNum")
